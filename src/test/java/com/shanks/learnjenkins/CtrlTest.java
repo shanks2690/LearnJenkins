f44f0bf574dbe5b0dba54c1418989a61fe195ad9
shanks2690 <142298930+shanks2690@users.noreply.github.com>
2024-12-19 00:43:20 +0530

    mvn clean install

---
+++ src/test/java/com/shanks/learnjenkins/CtrlTest.java
@@ -0,0 +1,30 @@
+package com.shanks.learnjenkins;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CtrlTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void testHelloEndpoint() {
+        // Arrange
+        String url = "http://localhost:" + port + "/api/hello";
+
+        // Act
+        String response = this.restTemplate.getForObject(url, String.class);
+
+        // Assert
+        assertThat(response).isEqualTo("Hello orld!");
+    }
+}
